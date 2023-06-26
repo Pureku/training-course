2b329d8d8573ab0a36fe8abec4fd2a0f87bb2826
bunnyDev <settawuts17@gmail.com>
2023-06-26 13:20:39 +0700

    add img

--- src/components/introcourse.component.js
+++ src/components/introcourse.component.js
@@ -6,8 +6,17 @@ const IntroCourse = () => {
     <>
       <div className="box-border min-w-0">
         <div className="intcosp ">
-          <div>
-            <h1 className="lg:text-5xl align-center">Course Training</h1>
+          <div className="intcosph">
+            <div>
+              <h1 className="lg:text-5xl align-center">Course Training</h1>
+              <p className="text-xl">Welcome to New Training Course</p>
+            </div>
+            <div>
+              <img
+                className="imgIntroCos"
+                src="https://raw.githack.com/Pureku/training-course/main/src/img/koosh-brain.webp?raw=true"
+              ></img>
+            </div>
           </div>
         </div>
       </div>
